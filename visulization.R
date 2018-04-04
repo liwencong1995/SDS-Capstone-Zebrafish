df55aa07470056a77d712d065145407d752762b5
Priscilla Li <priscilla@Priscillas-MacBook-Air.local>
2018-04-03 22:49:01 -0400

    added landmark coordinates

--- visulization.R
+++ visulization.R
@@ -46,31 +46,29 @@ landmark_ZRF_w_index <- landmark_ZRF_w_index %>%
   rename(sample_index = Index,
          landmark_index = id)
 fwrite(landmark_ZRF_w_index, "data/landmark_ZRF_w_index.csv")
+
+
+
 #--------------------------------------------------------------------
 #read in data needed to create vis
 r1_AT <- fread("results/r1_AT.csv")
 r1_ZRF <- fread("results/r1_ZRF.csv")
-landmark_AT <- fread("data/tidyLandmarks_AT_no_na.csv")
-landmark_label_ZRF <- fread("data/tidyLandmarks_ZRF.csv")
+landmark_label_raw <- fread("data/landmark_AT_w_index_no_na.csv")
 
-landmark_AT <- landmark_AT%>%
+landmark_AT <- landmark_label_raw%>%
   arrange(sample_index) %>%
-  select(-landmark_index, -V1) %>%
-  unique() 
+  select(-unique_key, -V1)
 
 landmark_AT_test <- landmark_AT %>%
   group_by(sample_index, min_alpha, min_theta)%>%
   summarise(N=n())
-  
+#passed the test
 
 test_num_sample <- landmark_AT%>%
   group_by(landmark_index, sample_index) %>%
   summarise(N=n()) %>%
   filter(N > 1)
 landmark_AT_cleaned <- unique(landmark_AT[ , 3:10 ] )
-
-
-
 
 #--------------------------------AT---------------------------------
 #label each landmark with (x,y) format
@@ -102,46 +100,6 @@ max(test_AT_y$N)
 min(test_AT_y$N)
 # there are only 19 landmarks in each theta range
 
-# Output landmark label for AT
-landmark_label_AT <- landmark_label_AT %>%
-  select(landmark_index, x, y)
-landmark_label_AT <- landmark_label_AT[2:4]
-fwrite(landmark_label_AT, "data/landmark_label_AT.csv")  
-
-#--------------------------------ZRF---------------------------------
-#label each landmark with (x,y) format
-# x = row number
-#y = column number
-landmark_label_AT <- landmark_AT %>%
-  group_by(landmark_index) %>%
-  summarise(min_alpha = mean(min_alpha),
-            max_alpha = mean(max_alpha),
-            min_theta = mean(min_theta),
-            max_theta = mean(max_theta),
-            num = mean(num))
-# add x label
-landmark_label_AT <- landmark_label_AT %>% 
-  group_by(min_alpha) %>% mutate(x = row_number())
-landmark_label_AT <- landmark_label_AT %>% 
-  group_by(min_theta) %>% mutate(y = row_number())
-# double check that there are 8 landmarks in each alpha slice
-test_AT_x <- landmark_label_AT %>%
-  group_by(y) %>%
-  summarise(N = n())
-max(test_AT_x$N)
-min(test_AT_x$N)
-# double check that there are 30 landmarks in each theta range
-test_AT_y <- landmark_label_AT %>%
-  group_by(x) %>%
-  summarise(N = n())
-max(test_AT_y$N)
-min(test_AT_y$N)
-# there are only 19 landmarks in each theta range
-
-# Output landmark label for AT
-landmark_label_AT <- landmark_label_AT %>%
-  select(landmark_index, x, y)
-landmark_label_AT <- landmark_label_AT[2:4]
-fwrite(landmark_label_AT, "data/landmark_label_AT.csv")  
-  
-  
+# Output landmark label
+landmark_label_AT_output <- landmark_label_AT[, c(1,7:8)]
+fwrite(landmark_label_AT_output, "analysis/landmark_xy.csv")  
