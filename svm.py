4a98c4c30a98deb71741e03183888623a9f583ad
DejiaTang <nora-tang@hotmail.com>
2018-04-05 14:56:09 -0400

    run multiple

--- svm.py
+++ svm.py
@@ -94,40 +94,49 @@ def svm_classification(landmarks, index):
     return svc, ww, wm, mm, mw
 
 # Read data
-#landmarks = pd.read_csv('./data/tidyLandmarks_AT_no_na.csv')
-#landmarks = pd.read_csv('../data/tidyLandmarks_ZRF_no_na.csv')
-
-#landmarks = pd.read_csv('./data/landmark_AT_w_index_no_na.csv')
-landmarks = pd.read_csv('./data/landmark_ZRF_w_index_no_na.csv')
+landmarks = pd.read_csv('./data/landmark_AT_w_index_no_na.csv')
 
 sample_id = str(input("Please enter sample index: "))
+sample_id2 = str(input("Please enter the second sample index: "))
+sample_id3 = str(input("Please enter the third sample index: "))
+sample_id4 = str(input("Please enter the fourth sample index: "))
 
 result_file_name = str(input("Please enter result file name: "))
+result_file_name2 = str(input("Please enter the second result file name: "))
+result_file_name3 = str(input("Please enter the third result file name: "))
+result_file_name4 = str(input("Please enter the fourth result file name: "))
 
-result_file = open(result_file_name, 'w') 
-result_file.write('landmark_index, pred, ww, wm, mm, mw\n')
-result_file.close() 
+sample_ids = [sample_id, sample_id2, sample_id3, sample_id4]
+result_file_names = [result_file_name, result_file_name2, result_file_name3, result_file_name4]
 
-sample = landmarks[landmarks.sample_index==sample_id]
-landmark_ids = sample['landmark_index']
+for i in range(4):
+	result_file_name = result_file_names[i]
+	sample_id = sample_ids[i]
 
-results = []
-leave_one_out = landmarks[landmarks.sample_index!=sample_id]
-for l in landmark_ids.values:
-    print ("=======================================")
-    print ("landmark: ", str(l))
-    svc, ww, wm, mm, mw = svm_classification(leave_one_out, l)
-    if (svc is None):
-        print("One of the classes have too few samples for this landmark, so skipping it.")
-        continue
-    prediction = svc.predict(sample[sample.landmark_index==l][['pts', 'r']])
-    result = ', '.join(str(x) for x in [l, prediction[0], ww, wm, mm, mw]) + '\n'
-    results.append((l, prediction[0], ww, wm, mm, mw))
-    print(results)
+	result_file = open(result_file_name, 'w') 
+	result_file.write('landmark_index, pred, ww, wm, mm, mw\n')
+	result_file.close() 
 
-    result_file = open(result_file_name, 'a') 
-    result_file.write(result)
-    result_file.close() 
+	sample = landmarks[landmarks.sample_index==sample_id]
+	landmark_ids = sample['landmark_index']
+
+	results = []
+	leave_one_out = landmarks[landmarks.sample_index!=sample_id]
+	for l in landmark_ids.values:
+	    print ("=======================================")
+	    print ("landmark: ", str(l))
+	    svc, ww, wm, mm, mw = svm_classification(leave_one_out, l)
+	    if (svc is None):
+	        print("One of the classes have too few samples for this landmark, so skipping it.")
+	        continue
+	    prediction = svc.predict(sample[sample.landmark_index==l][['pts', 'r']])
+	    result = ', '.join(str(x) for x in [l, prediction[0], ww, wm, mm, mw]) + '\n'
+	    results.append((l, prediction[0], ww, wm, mm, mw))
+	    print(results)
+
+	    result_file = open(result_file_name, 'a') 
+	    result_file.write(result)
+	    result_file.close() 
 
 '''print ("=======================================")
 print("SAMPLE REPORT")
