b4a807019d1b152f448b5bda428c4e129ebccc2f
jyun38 <jyun38@smith.edu>
2018-04-07 20:11:46 -0400

    new tidyr files

--- tidyLandmark.R
+++ tidyLandmark.R
@@ -2,8 +2,8 @@ library(readr)
 library(dplyr)
 library(tidyr)
 
-AT_dataset <- read.csv("~/Desktop/SDS-Capstone-Zebrafish/data/AT_landmarks_3-28-18.csv", header=T, check.names = FALSE)
-ZRF_dataset <- read.csv("~/Desktop/SDS-Capstone-Zebrafish/data/ZRF_landmarks_3-29-18.csv", header=T, check.names = FALSE)
+AT_dataset <- read.csv("~/Desktop/SDS-Capstone-Zebrafish/data/AT_landmarks.csv", header=T, check.names = FALSE)
+ZRF_dataset <- read.csv("~/Desktop/SDS-Capstone-Zebrafish/data/ZRF_landmarks.csv", header=T, check.names = FALSE)
 
 gatherAT <- AT_dataset %>%
   gather("colname", "values", -Index)
@@ -21,12 +21,6 @@ spreadAT <- separateAT %>%
 
 gatherZRF <- ZRF_dataset %>%
   gather("colname", "values", -Index)
-
-#separateZRF <- gatherZRF[1: 16720,] %>%
-  #separate(col = "colname", 
-    #       into=c("min_alpha", "max_alpha", "min_theta", "max_theta", "num", "ptsOrR"),
-     #      sep = "_"
-  #)
 
 separateZRF <- gatherZRF[1: 23712,] %>%
   separate(col = "colname", 
@@ -52,13 +46,6 @@ resultZRF <- left_join(spreadZRF, stypeData_ZRF, by="Index")
 resultZRF$colname <- NULL
 resultZRF$ind <- NULL
 colnames(resultZRF)[colnames(resultZRF) == 'values'] <- 'stype'
-
-
-
-res <- left_join(try3, stypeData, by="Index")
-res$colname <- NULL
-res$ind <- NULL
-colnames(res)[colnames(res) == 'values'] <- 'stype'
 
 write.csv(resultZRF, file="~/Desktop/tidyLandmarks_ZRF_new.csv")
 
