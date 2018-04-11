46d5077699d97cf7f5e24c7054611aa8f7a1addb
Priscilla Li <priscilla@Priscillas-MacBook-Air.local>
2018-04-11 19:53:18 -0400

    Revised shinyApp

--- shiny.R
+++ shiny.R
@@ -8,16 +8,16 @@ library(ggplot2)
 # input: Sample Index
 index <- AT_landmarks[,c(1,306)]
 index <- index %>%
-  mutate(AT = paste0("r", Index, "_med_AT"),
-         ZRF = paste0("r", Index, "_med_ZRF"))
-list_of_indices <- c(index$AT, index$ZRF)
-
+  arrange(Index)
+list_of_indices <- c(index$Index)
 list_of_scores <- c("precision", "recall", "f1", "w_precision", "w_recall", "w_f1", "m_precision", "m_recall", "m_f1")
 landmark_xy <- fread("/Users/priscilla/Desktop/SDS Capstone/Zebrafish/analysis/landmark_xy.csv")
+list_of_channel <- c("AT", "ZRf")
 
 ui <- fluidPage(
   titlePanel(title=h4("Classification of Wildtype and Mutant Zebrafish Brains via Computational Method", 
-                      align="center")), 
+                      align="center")),
+  selectInput("channel", "Channel:", list_of_channel),
   selectInput("sampleindex", "Sample Index:", list_of_indices),
   selectInput("score", "Accuracy Measurement:", list_of_scores),
   mainPanel(plotOutput("plot2")) )
@@ -25,7 +25,7 @@ ui <- fluidPage(
 server <- function(input,output) {
   dat <- reactive({
     wd <- getwd()
-    dir <- paste0(wd, "/analysis/", input$sampleindex, "_result.csv")
+    dir <- paste0(wd, "/analysis/r", input$sampleindex, "_med_", input$channel, "_result.csv")
     test <- fread(dir)
     test <- test %>%
       left_join(landmark_xy, by="landmark_index")
